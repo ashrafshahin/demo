1c6b5586330395bef1742f59cd18c9f6295100b9
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2025-07-07 12:01:05 +0100

    Manual and Automatic Namta

---
+++ js/script.js
@@ -0,0 +1,67 @@
+
+document.write("</br>")
+document.write("MANUAL WAY")
+document.write("</br>")
+document.write("</br>")
+
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+var seven = 7;
+var eight = 8;
+var nine = 9;
+var thirteen = 13;
+
+for (var i = 1; i <= 10; i++) {
+    // document.write(namta + " x " + i + " = " + i * namta + " </br> ");
+    
+    for (var i = 1; i <= 10; i++) {
+        document.write(two + " x " + i + " = " + i * two + " </br> ");
+    }
+    for (var i = 1; i <= 10; i++) {
+        document.write(three + " x " + i + " = " + i * three + " </br> ");
+    }
+    for (var i = 1; i <= 10; i++) {
+        document.write(four + " x " + i + " = " + i * four + " </br> ");
+    }
+    for (var i = 1; i <= 10; i++) {
+        document.write(five + " x " + i + " = " + i * five + " </br> ");
+    }
+    for (var i = 1; i <= 10; i++) {
+        document.write(six + " x " + i + " = " + i * six + " </br> ");
+    }
+    for (var i = 1; i <= 10; i++) {
+        document.write(seven + " x " + i + " = " + i * seven + " </br> ");
+    }
+    for (var i = 1; i <= 10; i++) {
+        document.write(eight + " x " + i + " = " + i * three + " </br> ");
+    }
+    for (var i = 1; i <= 10; i++) {
+        document.write(nine + " x " + i + " = " + i * nine + " </br> ");
+    }
+    for (var i = 1; i <= 10; i++) {
+        document.write(thirteen + " x " + i + " = " + i * thirteen + " </br> ");
+    }
+}
+
+document.write("</br>")
+document.write("AUTOMATIC WAY")
+document.write("</br>")
+document.write("</br>")
+
+
+
+
+for (var namta = 50; namta <= 69; namta++) {
+    // document.write(namta + " x " + i + " = " + i * namta + " </br> ");
+
+    for (var i = 1; i <= 10; i++) {
+        document.write(namta + " x " + i + " = " + i * namta + " </br> ");
+        
+    }
+    
+}
+
+
